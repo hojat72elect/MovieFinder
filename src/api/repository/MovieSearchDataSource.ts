c619420b80782f2c9fed7cef3a55b68ab8b6b683
hojat <hojat72elect@gmail.com>
2024-02-23 19:38:15 -0500

    docs.

--- src/api/repository/MovieSearchDataSource.ts
+++ src/api/repository/MovieSearchDataSource.ts
@@ -3,7 +3,10 @@ import {ApiTmdbService, SearchParams} from "../ApiTmdbService";
 
 const searchMoviesEndpoint = `${baseUrl}/search/movie?api_key=${apiKey}`;
 
-
+/**
+ * This data source is for searching movies and getting them from online API.
+ * @param params
+ */
 export const searchMovies = (params: SearchParams) => {
     return ApiTmdbService(searchMoviesEndpoint, params);
 }
